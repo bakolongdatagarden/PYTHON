da0f5214cfc376fd8da0d0d5281bb1751ab94181
Nyambi Bakolong <bakolongdatagarden@proton.me>
2025-07-03 20:57:47 -0500

    Demonstrate use of multiple elif statements in program for child medication dosage.

--- kaggle/conditionals/kaggle_notes_conditionals.py
+++ kaggle/conditionals/kaggle_notes_conditionals.py
@@ -107,6 +107,7 @@ def conditional_statements_demo():
         print("\nPatient has 34C temperature:")
         print(evaluate_temp_with_elif(34))
 
+    # EXAMPLE CALCULATIONS
     def example_calculations():
         """
         We can also use conditional statements to perform different calculations.
@@ -129,13 +130,45 @@ def conditional_statements_demo():
         print(ana_taxes)
         print(bob_taxes)
 
+    # MULTIPLE ELIFS 
+    def multiple_elifs():
+        """
+        There's no limit to the number of "elif" statements you can use.
+
+        For instance, the next block of code calculates the dose of medication (in milliliters) to give a child, based on weight (in kilograms).
+        """
+        def get_dose(weight):
+            # Dosage is 1.25 ml for anyone under 5.2 kg
+            if weight < 5.2:
+                dose = 1.25
+            elif weight < 7.9:
+                dose = 2.5
+            elif weight < 10.4:
+                dose = 3.75
+            elif weight < 15.9:
+                dose = 5 
+            elif weight < 21.2:
+                dose = 7.5
+            # Dosage is 10ml for anyone 21.2 kg or over
+            else:
+                dose = 10
+            return dose
+            """
+            Below we will run our function.
+            - Once an "elif" statement evaluates to True and the code block is run, the function skips over all remaining "elif" and "else" statements.
+            - After skipping these, all that is left is the return statement, which returns the value of dose.
+            - The order of the elif statements does matter here. Reordering will return a very different result.
+            """
+        print(get_dose(12))
+
+
 
     # Call subfunctions here
     # if_statements()
     # if_else_statements()
     # if_elif_else_statements()
-    example_calculations()
-
+    # example_calculations()
+    multiple_elifs()
 
 if __name__ == "__main__":
 #    conditions_demo()
